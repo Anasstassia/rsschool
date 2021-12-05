d9d1fc6d9b463c1d150f2c9c5a769b7e3e20d587
Anasstassia <gubinaa75@gmail.com>
2021-12-05 21:22:22 +0300

    feat: add links in footer

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,10 @@ const baseConfig = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.svg$/,
+                loader: 'file-loader',
+            },
         ],
     },
     resolve: {
