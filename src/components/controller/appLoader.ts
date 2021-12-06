c637973555a0686a0619d8b3eb150b96911e7507
Anasstassia <gubinaa75@gmail.com>
2021-12-06 21:48:24 +0300

    feat: change url

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'a12ecd76842a4e57bfbd432a94be1a4c', // получите свой ключ https://newsapi.org/
         });
     }
