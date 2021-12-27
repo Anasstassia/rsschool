5f71fcbd9e718b0e5ad0c7c652e99e7e3fe11814
Anasstassia <gubinaa75@gmail.com>
2021-12-27 17:54:52 +0300

    feat: start drag and drop

--- christmas-task/src/favorites.ts
+++ christmas-task/src/favorites.ts
@@ -20,8 +20,6 @@ export const drawToys = (toys: ToysList) => {
                 if (count) {
                     count.textContent = `${el.count}`;
                 }
-                newToy.setAttribute('draggable', 'true');
-                newToy.setAttribute('id', 'i');
                 container?.append(newToy);
             }
             template?.remove();
@@ -37,10 +35,60 @@ export const drawToys = (toys: ToysList) => {
             if (count) {
                 count.textContent = `${el.count}`;
             }
-            newToy.setAttribute('draggable', 'true');
-            newToy.setAttribute('id', 'i');
+            newToy.querySelector('img')?.setAttribute('draggable', 'true');
+            newToy.querySelector('img')?.setAttribute('id', String(i));
+
             container?.append(newToy);
         });
         template?.remove();
+        const draggable = document.querySelectorAll<HTMLElement>('[draggable]');
+        const targets = document.querySelectorAll<HTMLElement>('[data-drop-target]');
+
+        for (let i = 0; i < draggable.length; i++) {
+            draggable[i].addEventListener('dragstart', handleDragStart);
+        }
+
+        for (let i = 0; i < targets.length; i++) {
+            targets[i].addEventListener('dragover', handleOverDrop);
+            targets[i].addEventListener('drop', handleOverDrop);
+            targets[i].addEventListener('dragenter', handleDragEnterLeave);
+            targets[i].addEventListener('dragleave', handleDragEnterLeave);
+        }
     }
 };
+function handleDragStart(e: DragEvent) {
+    console.log(e.target);
+    e.dataTransfer?.setData('text', (e.target as HTMLElement).id);
+}
+
+function handleDragEnterLeave(e: DragEvent) {
+    if (e.type == 'dragenter') {
+        (e.target as HTMLElement).className = 'drag-enter';
+    } else {
+        (e.target as HTMLElement).className = '';
+    }
+}
+
+function handleOverDrop(e: DragEvent) {
+    e.preventDefault();
+
+    if (e.type != 'drop') {
+        return;
+    }
+    const draggedId = e.dataTransfer?.getData('text');
+    const draggedEl = document.getElementById(draggedId || '');
+    console.log(draggedEl, draggedId);
+    if (draggedEl?.parentNode == e.target) {
+        (e.target as HTMLElement).className = '';
+        return;
+    }
+    if (draggedEl) {
+        draggedEl?.parentNode?.removeChild(draggedEl);
+        draggedEl.style.width = '55px';
+        draggedEl.style.height = '55px';
+        if ((e.target as HTMLElement)?.parentElement) {
+            (e.target as HTMLElement)?.parentElement?.appendChild(draggedEl);
+            // (e.target as HTMLElement)?.parentElement?.className = '';
+        }
+    }
+}
