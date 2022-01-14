9b0c391a0be609914100a004d91a527892ed2409
Anasstassia <gubinaa75@gmail.com>
2022-01-14 17:05:53 +0300

    init: add files to app-folder

---
+++ async-race-app/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+            {
+                test: /\.[sc]?[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.svg$|.ttf|.png/,
+                loader: 'file-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.ts'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/'),
+                    to: path.resolve(__dirname, 'dist/assets/'),
+                },
+            ],
+        }),
+    ],
+};
